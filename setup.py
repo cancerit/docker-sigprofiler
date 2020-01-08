0e5608ac1cf61f06701d44d47928cb283b1bf886
Jingwei Wang <jw32@sanger.ac.uk>
2020-01-08 10:37:12 +0000

    test

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup (
     author = 'Jingwei Wang', 
     url = 'https://github.com/cancerit/docker-sigprofiler', 
     author_email = 'cgphelp@sanger.ac.uk', 
-    license='AGPL',
+    license='agpl-3.0',
     version = docker_sigprofiler.version, 
     python_requires = '>= 3.6', 
     packages = ['docker_sigprofiler'], 
